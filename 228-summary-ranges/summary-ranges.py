f2aeabc1097c99b2c93eb87945f00b7c723c2e4e
Ultronoss <79258081+Ultronoss@users.noreply.github.com>
2024-01-25 23:00:36 +0530

    Time: 34 ms (76.20%) | Memory: 16.6 MB (57.75%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -1,22 +1,24 @@
 class Solution:
-    def convert_to_string(self,a,b):
-        if a==b:
-            return str(a)
-        return f"{a}->{b}"
-
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if len(nums)==0:
+        if len(nums) == 0:
             return []
-        res = []    
-        range=[]
-        c=nums[0]
-        for n in nums:
-            if n == c:
-                range.append(n)
-                c+=1
+        l = r = 0
+        ans = []
+        while r != len(nums) - 1:
+            if nums[r] == nums[r + 1] - 1:
+                r += 1
             else:
-                res.append(self.convert_to_string(range[0],range[-1]))
-                range=[n]
-                c=n+1
-        res.append(self.convert_to_string(range[0],range[-1]))
-        return res
+                if r == l:
+                    ans.append(str(nums[l]))
+                else:
+                    ans.append(f"{nums[l]}->{nums[r]}")
+                r += 1
+                l = r
+
+        if r == l:
+            ans.append(str(nums[l]))
+        else:
+            ans.append(f"{nums[l]}->{nums[r]}")
+            
+        return ans
+        
