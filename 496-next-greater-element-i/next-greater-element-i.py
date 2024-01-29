8982a98d1c23960ad3c222106c78140dbda8434c
Ultronoss <79258081+Ultronoss@users.noreply.github.com>
2024-01-29 18:06:35 +0530

    Time: 85 ms (20.45%) | Memory: 18.7 MB (18.68%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,20 +1,10 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        next_greater = {}
-        stack = []
+        def find_next_greater(num, nums):
+            if not nums:
+                return -1
+            if num < nums[0]:
+                return nums[0]
+            return find_next_greater(num, nums[1:])
 
-        # Iterate over nums2 in reverse order
-        for num in reversed(nums2):
-            # Pop elements from the stack which are less than the current element
-            while stack and stack[-1] < num:
-                stack.pop()
-            # If the stack is not empty, the top element is the next greater element
-            # If the stack is empty, there is no next greater element
-            next_greater[num] = stack[-1] if stack else -1
-            # Push the current element to the stack
-            stack.append(num)
-
-        # Return the next greater element for each element in nums1
-        return [next_greater[num] for num in nums1]
-
-        
+        return [find_next_greater(num, nums2[nums2.index(num):]) for num in nums1]
