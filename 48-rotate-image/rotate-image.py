cadc7b6bcc0de732fd58eff19debb580e61cfb2f
Ultronoss <79258081+Ultronoss@users.noreply.github.com>
2024-02-02 12:45:58 +0530

    Time: 33 ms (92.00%) | Memory: 16.6 MB (68.45%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,12 +3,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        def rotateSingle(x, y, length, offset):
+            tmp = matrix[y][x + offset] #1
+            matrix[y][x + offset] = matrix[y + length - offset][x] #13 -> 1
+            matrix[y + length - offset][x] = matrix[y + length][x + length - offset] #12 -> 13
+            matrix[y + length][x + length - offset] = matrix[y + offset][x + length] #10 -> 12
+            matrix[y + offset][x + length] = tmp #12 <- tmp
+   
         n = len(matrix)
-        # Transpose the matrix
-        for i in range(n):
-            for j in range(i, n):
-                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-
-        # Reverse each row
-        for i in range(n):
-            matrix[i] = matrix[i][::-1]
+        i = 0
+        l = n - 1
+        while(l > 0):
+            for j in range(l):
+                rotateSingle(i,i,l,j)
+            l -= 2
+            i += 1
