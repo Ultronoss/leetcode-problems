0571854e9bf1bcb0db1dee84d975ef9671408ce8
Ultronoss <79258081+Ultronoss@users.noreply.github.com>
2024-02-02 12:36:12 +0530

    Time: 163 ms (39.82%) | Memory: 17.6 MB (73.72%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,11 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        maxPos, end, step = 0, 0, 0
-        for i in range(n - 1):
-            if maxPos >= i:
-                maxPos = max(maxPos, i + nums[i])
-                if i == end:
-                    end = maxPos
-                    step += 1
-        return step
+        result = 0
+        l = 0
+        r = 0 # initiate left and right index pointer for each jump
+
+        while r < len(nums) - 1:
+            far = 0
+            for i in range(l, r + 1):
+                far = max(far, i + nums[i])
+
+            l = l + 1
+            r = far
+            result += 1
+
+        return result
