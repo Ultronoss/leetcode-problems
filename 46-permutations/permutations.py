981b63706f913f2da7cb00aaf3778bd60f2a6c4f
Ultronoss <79258081+Ultronoss@users.noreply.github.com>
2024-02-07 21:55:35 +0530

    Time: 31 ms (98.01%) | Memory: 16.7 MB (64.41%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,11 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def back(nums, path):
-            if not nums:
-                res.append(path)
-                return
-            for i in range(len(nums)):
-                back(nums[:i] + nums[i + 1:], path + [nums[i]])
-        res = []
-        back(nums, [])
-        return res
+
+        ans = []
+        perm = []
+
+        def dfs(i): 
+            if i == len(nums): 
+                ans.append(perm[:])
+                return 
+            
+            for num in nums: 
+                if num not in perm: 
+                    perm.append(num)
+                    dfs(i+1)
+                    perm.pop()
+            
+        
+        dfs(0)
+        return ans
