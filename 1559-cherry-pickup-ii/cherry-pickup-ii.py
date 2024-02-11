4b6d84b4717ac49e6358c9b0508b429b27c083c1
Ultronoss <79258081+Ultronoss@users.noreply.github.com>
2024-02-11 18:56:13 +0530

    Time: 307 ms (99.75%) | Memory: 16.9 MB (98.73%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,22 +1,23 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        dp = [[[-1]*cols for _1 in range(cols)] for _2 in range(rows)]
-        
-        def helper(r, c1, c2):
-            if r == rows:
-                return 0
-            if dp[r][c1][c2] != -1:
-                return dp[r][c1][c2]
-            result = 0
-            for new_c1 in [c1, c1+1, c1-1]:
-                for new_c2 in [c2, c2+1, c2-1]:
-                    if new_c1>=0 and new_c1<cols and new_c2>=0 and new_c2<cols:
-                        result = max(result, helper(r+1, new_c1, new_c2))
-            result += grid[r][c1]
-            if c1 != c2:
-                result += grid[r][c2]
-            dp[r][c1][c2] = result
-            return result
-        
-        return helper(0, 0, cols-1)
+        m, n = len(grid), len(grid[0])
+        if n == 2:
+            return sum(sum(g) for g in grid)
+        elif n == 3:
+            return sum(sum(g)-min(g) for g in grid)
+
+        prev, dirs = [[0]*n for _ in range(n)], {(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)}
+        for i in range(m-1, -1, -1):
+            cur = [[0]*n for _ in range(n)]
+            for j in range(min(i+1, n)):
+                for k in range(max(j, n-i-1), n):
+                    for dir in dirs:
+                        s, t = j+dir[0], k+dir[1]
+                        if 0 <= s <= t < n and cur[j][k] < prev[s][t]:
+                            cur[j][k] = prev[s][t]
+                    cur[j][k] += grid[i][j] + (0 if j == k else grid[i][k])
+            prev = cur
+        return prev[0][-1]
+                    
+
+
